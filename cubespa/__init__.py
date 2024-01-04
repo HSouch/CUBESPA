6049a0aab32517db142ba10c55ca70db7998867a
HSouch <harrison.souchereau@yale.edu>
2024-01-04 00:52:28 -0400

    Added analysis and modeling imports

--- cubespa/__init__.py
+++ cubespa/__init__.py
@@ -1,14 +1,14 @@
 
+from .analysis import *         # Plots for flux analysis
+from .modeling import *
 
-from .channel_maps import *     # Module to handle channel maps
-from .cubespa import *
+from .cubespa import *          # Module with all CubeSPA object functions
 from .data import *             # Data handling modules
-from .moment_maps import *      # Module for plotting moment maps
 from .overlays import *         # Module for reprojection and creating overlays
 from .pvds import *             # Module for position velocity diagrams (PVDs)
 from .spectra import *          # Module for generating and analyzing spectra
 from .utils import *            # All miscellaneous utility modules
 
 
+__version__ = "0.0.8"
 
-__version__ = "0.0.5"
