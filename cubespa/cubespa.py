72410d0e5fcd099a94761bd3434b5dbaf7b44e07
HSouch <harrison.souchereau@yale.edu>
2024-01-18 12:40:29 -0500

    Added vsys and better cube analysis handling

--- cubespa/cubespa.py
+++ cubespa/cubespa.py
@@ -14,6 +14,7 @@ class CubeSPA:
     def __init__(self, cube, data_index=0, 
                  mom_maps=None, additional_maps = [],
                  center = None, position_angle = None, eps=None,
+                 vsys = 0,
                  limits = None, 
                  plot_dir = None,
                  **kwargs) -> None:
@@ -35,16 +36,18 @@ class CubeSPA:
         
         self.cube = data.handle_data(cube, handler=data.load_data, data_index=data_index)
         
-        
-        self.cube_noise_level, self.cube_rms = utils.estimate_rms(self.cube.data, 
-                                                                  utils.check_kwarg("cmin", 5, kwargs), 
-                                                                  utils.check_kwarg("cmax", 5, kwargs))
+        if self.cube is not None:
+            self.cube_noise_level, self.cube_rms = utils.estimate_rms(self.cube.data, 
+                                                                    utils.check_kwarg("cmin", 5, kwargs), 
+                                                                    utils.check_kwarg("cmax", 5, kwargs))
 
         self.center = center
         self.position_angle = position_angle
         self.eps = eps
 
-        self.velocities = self.velocities_from_wcs()
+        self.velocities = self.velocities_from_wcs(vsys=vsys)
+        self.vsys = vsys
+        self.beam_area = self.get_beam_area()
 
         self.mom_maps = data.handle_data(mom_maps, handler=data.load_moment_maps, data_index=data_index)
         self.additional_maps = additional_maps
@@ -69,11 +72,27 @@ class CubeSPA:
             pass
 
     
-    def velocities_from_wcs(self):
+    def velocities_from_wcs(self, vsys=0):
         vmin = self.cube.wcs.wcs.crval[2]
         vdelt = self.cube.wcs.wcs.cdelt[2]
 
-        return np.array([vmin + i * vdelt for i in range(len(self.cube.data))]) / 1000
+        return (np.array([vmin + i * vdelt for i in range(len(self.cube.data))]) / 1000) - vsys
+    
+
+    def get_beam_area(self):
+        try:
+            header, w = self.cube.header, self.cube.wcs
+            
+            delt = np.mean(np.abs(w.wcs.cdelt[:2]))
+
+            bmin, bmaj = header["BMIN"] / delt , header["BMAJ"] / delt
+            
+            area = np.pi * bmaj * bmin / (4 * np.log(2))
+        except Exception as e:
+            print("Failed to get beam area:", e)
+            area = 1
+            
+        return area
     
     # UTILITY FUNCTIONS
     def plot_moment_maps(self, use_limits=True, **kwargs):
